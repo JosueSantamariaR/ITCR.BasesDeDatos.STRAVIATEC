d8e22df0d0e9c93a4a831a9bac1507f6bfd0201c
José Fabián Mendoza Mata <49073441+JoseMendozaMata@users.noreply.github.com>
2022-05-09 01:42:45 -0600

    Controllers and classes for User-Activity

---
+++ ApiStraviaTec/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiStraviaTec
+{
+    public class Activity
+    {
+        public int id { get; set; }
+        public string date { get; set; }
+        public string hour { get; set; }
+        public string duration { get; set; }
+        public string type { get; set; }
+        public int mileage { get; set; }
+        public int traveled { get; set; }
+    }
+}
---
+++ ApiStraviaTec/Controllers/ActivityController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiStraviaTec.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var activity = new Activity
+            {
+                id = 1,
+                date = new DateTime(2020, 7, 25).ToString("dd-MM-yyyy"),
+                hour = new DateTime(2020, 7, 25).ToString("HH:mm:ss"),
+                duration = "2 hours",
+                mileage = 200,
+                traveled = 150,
+                type = "longitud"
+            };
+
+            return Ok(activity);
+        } 
+    }
+}
