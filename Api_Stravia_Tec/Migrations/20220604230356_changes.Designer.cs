203274c1062973b8e1b19854367b65d6ead08749
José Fabián Mendoza Mata <49073441+JoseMendozaMata@users.noreply.github.com>
2022-06-04 17:04:28 -0600

    Migration

--- Api_Stravia_Tec/Migrations/20220604230356_changes.Designer.cs
+++ Api_Stravia_Tec/Migrations/20220604230356_changes.Designer.cs
@@ -4,6 +4,7 @@ using Api_Stravia_Tec.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Api_Stravia_Tec.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220604230356_changes")]
+    partial class changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
